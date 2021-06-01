84e771bd8b125c43f77f04bc7caf72ef9ab4f6ad
bishwa14370 <bishwajit@verificient.com>
2021-06-01 18:33:30 +0530

    Initial Commit

---
+++ ProctortrackSDK/ProctortrackSDK.h
@@ -0,0 +1,18 @@
+//
+//  ProctortrackSDK.h
+//  ProctortrackSDK
+//
+//  Created by Bishwajit on 01/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProctortrackSDK.
+FOUNDATION_EXPORT double ProctortrackSDKVersionNumber;
+
+//! Project version string for ProctortrackSDK.
+FOUNDATION_EXPORT const unsigned char ProctortrackSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProctortrackSDK/PublicHeader.h>
+
+
